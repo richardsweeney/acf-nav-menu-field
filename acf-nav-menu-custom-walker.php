941ffc204fbc7901ff1a96fd3a16794e89c2925a
Richard Sweeney <theorobman@gmail.com>
2013-08-30 12:24:18 +0200

    Don't show the pages' link in the menu. We can't set the link as its own parent!

--- acf-nav-menu-custom-walker.php
+++ acf-nav-menu-custom-walker.php
@@ -18,6 +18,15 @@ class Acf_Nav_Menu_Custom_Walker extends Walker_Nav_Menu {
     }
 
     function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+        global $post, $wpdb;
+
+        $sql          = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_menu_item_object_id' AND meta_value='%d'";
+        $menu_item_id = $wpdb->get_var( $wpdb->prepare( $sql, $post->ID ) );
+
+        // Don't show the current page menu item in the select list.
+        if ( $object->ID == $menu_item_id )
+            return;
+
         $indent = str_repeat( "-", $depth ) . ' ';
 
         $output .= sprintf( '<option %s value="%d">%s%s', selected( $this->selected, $object->ID, false ), $object->ID, $indent, $object->title );
